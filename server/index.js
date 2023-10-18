d6048d0744826c57df0049b4be973ea31253eaa8
SimonPercy <mmmRaliphada809@gmail.com>
2023-10-18 21:27:21 +0200

    database connected

--- server/index.js
+++ server/index.js
@@ -1,6 +1,25 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
 
-app.listen(5174 ,() => {
-    console.log("running on 5174");
-})
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "hfgame",
+});
+app.get("/", (req, res) => {
+  const sqlInsert = "INSERT INTO hf (name) VALUES ('THAT GUY');";
+  db.query(sqlInsert, (err, results) => {
+    if (err) {
+      console.error("Database error: " + err.message);
+      res.status(500).send("Internal Server Error");
+    } else {
+      res.send("Record inserted successfully.");
+    }
+  });
+});
+
+app.listen(5174, () => {
+  console.log("running on 5174");
+});
