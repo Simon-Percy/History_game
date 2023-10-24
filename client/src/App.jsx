fcef2aa0df04bc588bd49006bc4911716b943716
SimonPercy <mmmRaliphada809@gmail.com>
2023-10-24 21:55:24 +0200

    set up form with tiers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,38 @@ import "./App.css";
 import Cards from "./Cards";
 
 function App() {
-  const [loading, SetLoading] = useState(true);
-  const [info, SetInfo] = useState(null);
+  /*loading screen skeletons*/
+  const [loading, setLoading] = useState(true);
+  /*info being fetched from API*/
+  const [info, setInfo] = useState(null);
+  /*tiers for rating*/
+  const [tiers, setTiers] = useState({
+    tier1: 0,
+    tier2: 0,
+    tier3: 0,
+    tier4: 0,
+    tier5: 0,
+    tier6: 0,
+    tier7: 0,
+  });
+  /*average tier for each figure*/
+  const [avgTier, setAvgTier] = useState(0);
+
+  /*add all the tiers for that figure and find the average*/
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const total = Object.values(tiers).reduce(
+      (acc, currentValue) => acc + currentValue
+    );
+    const average = total / 7;
+    setAvgTier(average);
+    console.log(average);
+  };
+
+  const handleChange = (e) => {
+    setTiers({ ...tiers, [e.target.id]: parseInt(e.target.value) });
+  };
 
   useEffect(() => {
     async function fetchfigures() {
@@ -14,31 +44,34 @@ function App() {
           return `Error : Status : ${response.status}`;
         }
         const data = await response.json();
-        SetInfo(data);
+        setInfo(data);
       } catch (error) {
         console.error(`Error ${error}`);
       }
     }
     fetchfigures();
-    SetLoading(false);
+    setLoading(false);
   }, []);
-  const boxs = [1, 2, 3, 4, 5];
-  const [cardcount, SetCardsCount] = useState(1);
 
   return (
     <div className="box-wrapper">
       {loading && <p>Loading...</p>}
-      {info &&
-        info.map((element) => {
-          return (
-            <Cards
-              name={element.name}
-              title={element.title}
-              imageUrl={element.image_url}
-              key={element.id}
-            />
-          );
-        })}
+
+      <form onSubmit={handleSubmit}>
+        {info &&
+          info.map((element) => {
+            return (
+              <Cards
+                name={element.name}
+                title={element.title}
+                imageUrl={element.image_url}
+                key={element.id}
+                handleChange={handleChange}
+              />
+            );
+          })}
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 }
--- client/src/Cards.jsx
+++ client/src/Cards.jsx
@@ -1,59 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Cards = ({ name, imageUrl, title }) => {
+const Cards = ({ name, imageUrl, title, handleChange, handleSubmit }) => {
   return (
     <div className="cards-box">
       <h1>{name}</h1>
       <img className="image" src={imageUrl} />
       <p className="title">{title}</p>
-      <form name="tierform">
+      <form>
         <input
+          onChange={handleChange}
           type="radio"
+          id="tier1"
           className="tier-buttons"
           name="tiers"
-          value="tier7"
+          value="1"
         />
         <label>Tier 1</label>
         <input
+          onChange={handleChange}
           type="radio"
+          id="tier2"
           className="tier-buttons"
           name="tiers"
-          value="tier1"
+          value="2"
         />
         <label>Tier 2</label>
         <input
+          onChange={handleChange}
           type="radio"
+          id="tier3"
           className="tier-buttons"
           name="tiers"
-          value="tier2"
+          value="3"
         />
         <label>Tier 3</label>
         <input
+          onChange={handleChange}
           type="radio"
+          id="tier4"
           className="tier-buttons"
           name="tiers"
-          value="tier3"
+          value="4"
         />
         <label>Tier 4</label>
         <input
+          onChange={handleChange}
           type="radio"
+          id="tier5"
           className="tier-buttons"
           name="tiers"
-          value="tier4"
+          value="5"
         />
         <label>Tier 5</label>
         <input
+          onChange={handleChange}
           type="radio"
+          id="tier6"
           className="tier-buttons"
           name="tiers"
-          value="tier5"
+          value="6"
         />
         <label>Tier 6</label>
         <input
+          onChange={handleChange}
           type="radio"
+          id="6"
           className="tier-buttons"
           name="tiers"
-          value="tier6"
+          value="7"
         />
         <label>Tier 7</label>
       </form>
